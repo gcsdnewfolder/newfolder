a97b6b4f779d7f558d28183d18b871504dcc15fe
gcsdnewfolder <gcsdnewfolder@gmail.com>
2016-05-13 10:02:14 -0400

    Update nf.bat

--- nf.bat
+++ nf.bat
@@ -1,6 +1,6 @@
 :: New Folder Menu Script
 
-SET version=1.0
+SET version=0.1
 TITLE New Folder Menu %version%
 ECHO OFF
 CLS
@@ -40,7 +40,7 @@ IF %version%==%newestversion% (
 
 IF %version% LSS %newestversion% (
   ::If the current installed version is older than the latest version
-  echo Your version is out of date!
+  goto UPDATE
   )
 
 :: Menu Start
@@ -94,6 +94,47 @@ CLS
 :MUSIC
 cd M:\Olympia\Student\New folder\New folder
 start Music.vbs
+GOTO MENU
+CLS
+
+:UPDATE
+  echo Your version is out of date!
+  echo Updating now!
+  rem Windows has no built-in wget or curl, so generate a VBS script to do it:
+  rem -------------------------------------------------------------------------
+  set DLOAD_SCRIPT=%temp%\download.vbs
+  echo Option Explicit                                                    >  %DLOAD_SCRIPT%
+  echo Dim args, http, fileSystem, adoStream, url, target, status         >> %DLOAD_SCRIPT%
+  echo.                                                                   >> %DLOAD_SCRIPT%
+  echo Set args = Wscript.Arguments                                       >> %DLOAD_SCRIPT%
+  echo Set http = CreateObject("WinHttp.WinHttpRequest.5.1")              >> %DLOAD_SCRIPT%
+  echo url = args(0)                                                      >> %DLOAD_SCRIPT%
+  echo target = args(1)                                                   >> %DLOAD_SCRIPT%
+  echo WScript.Echo "Getting '" ^& target ^& "' from '" ^& url ^& "'..."  >> %DLOAD_SCRIPT%
+  echo.                                                                   >> %DLOAD_SCRIPT%
+  echo http.Open "GET", url, False                                        >> %DLOAD_SCRIPT%
+  echo http.Send                                                          >> %DLOAD_SCRIPT%
+  echo status = http.Status                                               >> %DLOAD_SCRIPT%
+  echo.                                                                   >> %DLOAD_SCRIPT%
+  echo If status ^<^> 200 Then                                            >> %DLOAD_SCRIPT%
+  echo    WScript.Echo "FAILED to download: HTTP Status " ^& status       >> %DLOAD_SCRIPT%
+  echo    WScript.Quit 1                                                  >> %DLOAD_SCRIPT%
+  echo End If                                                             >> %DLOAD_SCRIPT%
+  echo.                                                                   >> %DLOAD_SCRIPT%
+  echo Set adoStream = CreateObject("ADODB.Stream")                       >> %DLOAD_SCRIPT%
+  echo adoStream.Open                                                     >> %DLOAD_SCRIPT%
+  echo adoStream.Type = 1                                                 >> %DLOAD_SCRIPT%
+  echo adoStream.Write http.ResponseBody                                  >> %DLOAD_SCRIPT%
+  echo adoStream.Position = 0                                             >> %DLOAD_SCRIPT%
+  echo.                                                                   >> %DLOAD_SCRIPT%
+  echo Set fileSystem = CreateObject("Scripting.FileSystemObject")        >> %DLOAD_SCRIPT%
+  echo If fileSystem.FileExists(target) Then fileSystem.DeleteFile target >> %DLOAD_SCRIPT%
+  echo adoStream.SaveToFile target                                        >> %DLOAD_SCRIPT%
+  echo adoStream.Close                                                    >> %DLOAD_SCRIPT%
+  echo.                                                                   >> %DLOAD_SCRIPT%
+  rem -------------------------------------------------------------------------
+  cscript //Nologo %DLOAD_SCRIPT% https://raw.githubusercontent.com/gcsdnewfolder/newfolder/master/nf.bat "Menu - %newestversion%.bat"
+  del "%temp%\download.vbs"
 GOTO MENU
 CLS
 :: Execute Menu Options End
