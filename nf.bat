d8d2897476973b43fa8380f817fe29418601a15f
gcsdnewfolder <gcsdnewfolder@gmail.com>
2016-06-01 08:43:06 -0400

    Update of nf.bat
    Added pages, dynamic page return, and launch password

--- nf.bat
+++ nf.bat
@@ -1,5 +1,6 @@
 :: New Folder Menu Script
 
+SET page=1
 SET version=1.0
 TITLE New Folder Menu %version%
 ECHO OFF
@@ -56,12 +57,36 @@ IF %version% LSS %newestversion% (
   goto UPDATE
   )
 
+GOTO PAGE%page%
+
 :: Menu Start
 :MENU
 CLS
 ECHO.
+ECHO ERROR 404 Page Not Found.
+ECHO.
+ECHO ERROR INVALID PAGE
+ECHO ERROR INVALID PAGE
+ECHO ERROR INVALID PAGE
+ECHO ERROR INVALID PAGE
+ECHO ERROR INVALID PAGE
+ECHO ERROR INVALID PAGE
+ECHO.
+SET /P M=Type 1 then press ENTER:
+IF %M%==1 GOTO PAGE%page%
+IF ELSE (
+	GOTO MENU
+	)
+:: Menu End
+
+:: Page 1 Start
+:PAGE1
+SET page=1
+CLS
+ECHO Page 1
+ECHO.
 ECHO ...............................................
-ECHO PRESS 1, 2, 3, 4 OR 5 to select your task, or 6 to EXIT.
+ECHO PRESS 1, 2, 3, 4, 5 OR 6 to select your task.
 ECHO ...............................................
 ECHO.
 ECHO 1 - Open Notepad
@@ -69,7 +94,7 @@ ECHO 2 - Open Calculator
 ECHO 3 - Open Internet Explorer
 ECHO 4 - Open Music
 ECHO 5 - Open CMD
-ECHO 6 - EXIT
+ECHO 6 - Next Page
 ECHO.
 SET /P M=Type 1, 2, 3, 4, 5, or 6 then press ENTER:
 IF %M%==1 GOTO NOTE
@@ -77,41 +102,96 @@ IF %M%==2 GOTO CALC
 IF %M%==3 GOTO IE
 IF %M%==4 GOTO MUSIC
 IF %M%==5 GOTO CMD
-IF %M%==6 GOTO EOF
+IF %M%==6 GOTO PAGE2
 IF ELSE (
-	GOTO MENU
+	GOTO PAGE1
 	)
-:: Menu End
+:: Page 1 End
+
+:: Page 2 Start
+:PAGE2
+SET page=2
+CLS
+ECHO Page 2
+ECHO.
+ECHO ...............................................
+ECHO PRESS 1, 2, 3, 4, 5 OR 6 to select your task, or 7 to Exit.
+ECHO ...............................................
+ECHO.
+ECHO 1 - Open Chrome
+ECHO 2 - Open Paint
+ECHO 3 - Open Internet Explorer(In Private)
+ECHO 4 - Open Chrome(Incognito)
+ECHO 5 - Open New Folder(Coming Soon)
+ECHO 6 - Previous Page
+ECHO 7 - Exit
+ECHO.
+SET /P M=Type 1, 2, 3, 4, 5, or 6 then press ENTER:
+IF %M%==1 GOTO CHROME
+IF %M%==2 GOTO PAINT
+IF %M%==3 GOTO IEIP
+IF %M%==4 GOTO CHROMEI
+IF %M%==5 GOTO PAGE2
+IF %M%==6 GOTO PAGE1
+IF %M%==7 GOTO EOF
+IF ELSE (
+	GOTO PAGE2
+	)
+:: Page 2 End
 
 :: Execute Menu Options Start
 :NOTE
 cd %windir%\system32\notepad.exe
 start notepad.exe
-GOTO MENU
+GOTO PAGE%page%
 CLS
 
 :CALC
 cd %windir%\system32\calc.exe
 start calc.exe
-GOTO MENU
+GOTO PAGE%page%
 CLS
 
 :IE
 cd "C:\Program Files\Internet Explorer\iexplore.exe"
 start iexplore.exe
-GOTO MENU
+GOTO PAGE%page%
 CLS
 
 :CMD
 cd %windir%\system32\cmd.exe
 start cmd.exe
-GOTO MENU
+GOTO PAGE%page%
 CLS
 
 :MUSIC
 cd M:\Olympia\Student\New folder\New folder
 start Music.vbs
-GOTO MENU
+GOTO PAGE%page%
+CLS
+
+:IEIP
+cd "C:\Program Files\Internet Explorer\iexplore.exe"
+start "" "iexplore.exe" -private
+GOTO PAGE%page%
+CLS
+
+:CHROME
+cd "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start chrome.exe
+GOTO PAGE%page%
+CLS
+
+:CHROMEI
+cd "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" "chrome.exe"  -incognito
+GOTO PAGE%page%
+CLS
+
+:PAINT
+cd %windir%\system32\mspaint.exe
+start mspaint.exe
+GOTO PAGE%page%
 CLS
 
 :UPDATE
@@ -152,6 +232,6 @@ CLS
   rem -------------------------------------------------------------------------
   cscript //Nologo %DLOAD_SCRIPT% https://raw.githubusercontent.com/gcsdnewfolder/newfolder/master/nf.bat "Menu - %newestversion%.bat"
   del "%temp%\download.vbs"
-GOTO MENU
+GOTO PAGE%page%
 CLS
 :: Execute Menu Options End
